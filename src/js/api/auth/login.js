d62cea70ded1b4030e148892d7b7b85d23d741c4
TinyFridge <146342506+TinyFridge@users.noreply.github.com>
2025-02-10 16:40:44 +0100

    Working on javascript for the loginpage

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,14 @@
-export async function login({ email, password }) {}
+document.addEventListener("DOMContentLoaded",  () => {
+    const loginForm = document.getElementById("login-form");
+    const loginSection = document.getElementById("login-section");
+    const loginError = document.getElementById("login-error");
+    const logoutBtn = document.getElementById("logout-btn");
+    const loginLink = document.getElementById("login-link");
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+        loginSection.style.display = "none";
+        logoutBtn.classList.remove("hidden");
+        loginLink.classList.add("hidden");
+    )
