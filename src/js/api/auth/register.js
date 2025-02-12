2b34abc05d8db16eaa214c4b513ff93de9376ab7
TinyFridge <146342506+TinyFridge@users.noreply.github.com>
2025-02-12 14:13:49 +0100

    Update register.js

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,11 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+export function onRegister(event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").ariaValueMax.trim();
+  const password = document.getElementById("password").ariaValueMax.value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const errorElement = document.getElementById("register-error");
+
+  errorElement.classList.add("hidden");
+  errorElement.textContent = "";
+}
